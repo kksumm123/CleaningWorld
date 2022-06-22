3470c5f01e4f595d3e85cd8232ad0ff12ea342ff
kksumm123 <82883758+kksumm123@users.noreply.github.com>
2022-06-22 17:13:39 +0900

    1. RecycleObject.cs, 추가 및 작성완료 2. ObjectPoolSystem.cs, 추가 및 작성완료

---
+++ Assets/Scripts/Common/ObjectPool/ObjectPoolSystem.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolSystem
+{
+    int LAST_INDEX => objectPool.Count - 1;
+
+    RecycleObject recycleObjectPrefab;
+    RecycleObject tempObject;
+    List<RecycleObject> objectPool = new List<RecycleObject>();
+    Transform parent;
+
+    int defaultPoolSize;
+
+    public ObjectPoolSystem(RecycleObject recycleObjectPrefab, int defaultPoolSize, Transform parent)
+    {
+        if (defaultPoolSize <= 0)
+        {
+            defaultPoolSize = 1;
+        }
+
+        this.recycleObjectPrefab = recycleObjectPrefab;
+        this.defaultPoolSize = defaultPoolSize;
+        this.parent = parent;
+    }
+
+    void CreateObject()
+    {
+        for (int i = 0; i < defaultPoolSize; i++)
+        {
+            tempObject = MonoBehaviour.Instantiate(recycleObjectPrefab, parent);
+            tempObject.InitializedByObjectPoolSystem(Restore);
+            tempObject.gameObject.SetActive(false);
+            objectPool.Add(tempObject);
+        }
+    }
+
+    public RecycleObject Get()
+    {
+        if (objectPool.Count <= 0)
+        {
+            CreateObject();
+        }
+
+        tempObject = objectPool[LAST_INDEX];
+        objectPool.RemoveAt(LAST_INDEX);
+        tempObject.gameObject.SetActive(true);
+        return tempObject;
+    }
+
+    public void Restore(RecycleObject recycleObject)
+    {
+        recycleObject.gameObject.SetActive(false);
+        objectPool.Add(recycleObject);
+    }
+}
---
+++ Assets/Scripts/Common/ObjectPool/RecycleObject.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecycleObject : MonoBehaviour
+{
+    Action<RecycleObject> restore;
+
+    public void InitializedByObjectPoolSystem(Action<RecycleObject> restore)
+    {
+        this.restore = restore;
+    }
+}
