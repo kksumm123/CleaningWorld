9cbab76dc2dcee39e0469b0d73d5e92525748e70
kksumm123 <kksumm123@gmail.com>
2022-07-31 00:31:48 +0900

    1. SceneLoadSytem.cs, 추가 2. GameManager.cs, SceneLoadSystem 연동

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-   
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    public void LoadScene(string sceneName, LoadSceneMode loadSceneMode)
+    {
+        SceneLoadSystem.LoadScene(sceneName, loadSceneMode);
+    }
 }
---
+++ Assets/Scripts/SceneLoadSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoadSystem
+{
+    static bool isAbleToLoadScene = true;
+
+    private static void OnLoadedScene(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        SceneManager.sceneLoaded -= OnLoadedScene;
+        // 로드가 완료 되면 할 행동
+        isAbleToLoadScene = true;
+    }
+
+    private static IEnumerator AsynLoadScene(string sceneName, LoadSceneMode loadSceneMode)
+    {
+        if (isAbleToLoadScene == false)
+        {
+            yield break;
+        }
+
+        isAbleToLoadScene = false;
+
+        SceneManager.sceneLoaded += OnLoadedScene;
+        var process = SceneManager.LoadSceneAsync(sceneName, loadSceneMode);
+        yield return new WaitUntil(() => process.isDone);
+    }
+
+    public static void LoadScene(string sceneName, LoadSceneMode loadSceneMode)
+    {
+        // 잘못 사용하기도 어렵게 만들어라.
+        // 외부에서 코루틴을 직접 실행하게 하지 말고
+        // 내부에서 래핑하고 실행하도록 만들자
+        GameManager.Instance.StartCoroutine(AsynLoadScene(sceneName, loadSceneMode));
+    }
+}
