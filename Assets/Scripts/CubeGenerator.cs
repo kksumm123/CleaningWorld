efbaa32b582ed7331bdfbe8a4e8000002c0de9e1
kksumm123 <kksumm123@gmail.com>
2022-07-31 00:54:30 +0900

    1. Cube프리팹 추가 2. CubeGenerator.cs, 추가 및 연동 3. MainIsland 씬 저장

---
+++ Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour
+{
+    [SerializeField] Transform cubePrefab;
+
+    IEnumerator Start()
+    {
+        Debug.Assert(cubePrefab != null, "큐브 프리팹이 비어이따");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(cubePrefab,
+                        Vector3.zero + Vector3.right * i,
+                        Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
