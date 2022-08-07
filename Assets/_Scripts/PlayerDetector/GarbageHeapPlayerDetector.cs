1629015d6f04c1aa6203c3da86839afffc4137a6
kksumm123 <kksumm123@gmail.com>
2022-08-08 01:04:09 +0900

    1. System씬, GameResourcesManager 추가 2. PrefabResourcesInfoSO.asset, 프리팹 할당 3. PlayerDetector.cs, 위치 이동 및 구현 완료 4. GarbageHeapPlayerDetector.cs, 임시 작성 완료

---
+++ Assets/_Scripts/PlayerDetector/GarbageHeapPlayerDetector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageHeapPlayerDetector : PlayerDetector
+{
+    protected override void _OnTriggerEnter()
+    {
+        Debug.Log("힣힣히 오줌발싸 !");
+    }
+
+    protected override void _OnTriggerExit() { }
+}
---
+++ Assets/_Scripts/PlayerDetector/PlayerDetector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerDetector : MonoBehaviour
+{
+    [SerializeField] Collider mainCollider;
+
+    private void Start()
+    {
+        Debug.Assert(mainCollider != null, $"mainCollider is null", transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(AllTagAndLayer.Player) == false)
+        {
+            return;
+        }
+
+        _OnTriggerEnter();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(AllTagAndLayer.Player) == false)
+        {
+            return;
+        }
+
+        _OnTriggerExit();
+    }
+
+    protected abstract void _OnTriggerEnter();
+
+    protected abstract void _OnTriggerExit();
+}
