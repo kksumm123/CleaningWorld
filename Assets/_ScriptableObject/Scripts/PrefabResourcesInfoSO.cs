370bd1a7ca9933581fb911dabcce6ae6aa6889ea
kksumm123 <kksumm123@gmail.com>
2022-08-07 22:56:55 +0900

    PrefabResourcesInfoSO.cs, GetPrefab메소드 작성

--- Assets/_ScriptableObject/Scripts/PrefabResourcesInfoSO.cs
+++ Assets/_ScriptableObject/Scripts/PrefabResourcesInfoSO.cs
@@ -20,5 +20,44 @@ public class PrefabResourcesInfoSO : ScriptableObject
     [SerializeField] GarbageObject Platic1Prefab;
     [SerializeField] GarbageObject Platic2Prefab;
     [SerializeField] GarbageObject Platic3Prefab;
-}
 
+    public GarbageObject GetGarbageObjectPrefab(GarbageType garbageType)
+    {
+        switch (garbageType)
+        {
+            case GarbageType.Can1:
+                return can1Prefab;
+            case GarbageType.Can2:
+                return can2Prefab;
+            case GarbageType.Can3:
+                return can3Prefab;
+            case GarbageType.Food1:
+                return food1Prefab;
+            case GarbageType.Food2:
+                return food2Prefab;
+            case GarbageType.Food3:
+                return food3Prefab;
+            case GarbageType.Glass1:
+                return Glass1Prefab;
+            case GarbageType.Glass2:
+                return Glass2Prefab;
+            case GarbageType.Glass3:
+                return Glass3Prefab;
+            case GarbageType.Paper1:
+                return paper1Prefab;
+            case GarbageType.Paper2:
+                return paper2Prefab;
+            case GarbageType.Paper3:
+                return paper3Prefab;
+            case GarbageType.Plastic1:
+                return Platic1Prefab;
+            case GarbageType.Plastic2:
+                return Platic2Prefab;
+            case GarbageType.Plastic3:
+                return Platic3Prefab;
+            default:
+                Debug.Log($"PrefabResourcesInfoSO : 이게 호출되면 안됨, garbageType = {garbageType}");
+                return null;
+        }
+    }
+}
