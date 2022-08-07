fc7e7477ca93d4b9840b7cc4cf8fcf00fc52208a
kksumm123 <kksumm123@gmail.com>
2022-08-08 00:24:15 +0900

    FactoryManager.cs, GetGarbageObject() 메소드 추가

--- Assets/_Scripts/Manager/FactoryManager.cs
+++ Assets/_Scripts/Manager/FactoryManager.cs
@@ -10,4 +10,9 @@ public class FactoryManager : Singleton<FactoryManager>
     {
         prefabFactorySystem.Initialize(transform);
     }
+
+    public GarbageObject GetGarbageObject(GarbageType garbageType, Vector3 spawnPoint)
+    {
+        return prefabFactorySystem.GetGarbageObject(garbageType, spawnPoint);
+    }
 }
