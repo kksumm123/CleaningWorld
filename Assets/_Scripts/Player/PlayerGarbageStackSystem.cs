8581bf75e278041ac8ab178a1a6f1a37fe98bf58
kksumm123 <kksumm123@gmail.com>
2022-08-11 23:44:47 +0900

    [기능] GarbageStack 로직 개선

--- Assets/_Scripts/Player/PlayerGarbageStackSystem.cs
+++ Assets/_Scripts/Player/PlayerGarbageStackSystem.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-class MyStack<T> where T : GarbageObject
+class GarbageStack<T> where T : GarbageObject
 {
     readonly List<T> container = new List<T>();
     T tempItem;
@@ -14,8 +15,15 @@ class MyStack<T> where T : GarbageObject
         return container.Count();
     }
 
-    public void Push(T item)
+    public void Push(T item, Func<int, Vector3> getPosition)
     {
+        for (int i = 0; i < container.Count; i++)
+        {
+            container[i].transform.localPosition = getPosition(i);
+        }
+
+        item.transform.DOLocalMove(getPosition(container.Count), 0.5f);
+
         container.Add(item);
     }
 
@@ -37,10 +45,11 @@ class MyStack<T> where T : GarbageObject
 public class PlayerGarbageStackSystem
 {
     Player player;
-    MyStack<GarbageObject> myGarbages = new MyStack<GarbageObject>();
+    GarbageStack<GarbageObject> myGarbages = new GarbageStack<GarbageObject>();
 
     [SerializeField] Transform pivotCenter;
     [SerializeField] float garbageGap = 0.5f;
+    [SerializeField] int orderCount = 3;
 
     public void Initialize(Player player)
     {
@@ -51,10 +60,16 @@ public class PlayerGarbageStackSystem
     public void OnGarbageHeap(GarbageObject garbageObject)
     {
         garbageObject.transform.SetParent(pivotCenter);
-        garbageObject.transform.position = pivotCenter.position
-                                                + (myGarbages.Count() * garbageGap * Vector3.up);
         garbageObject.transform.localRotation = Quaternion.identity;
-        myGarbages.Push(garbageObject);
+
+        myGarbages.Push(garbageObject, GetPosition);
+
+        Vector3 GetPosition(int index)
+        {
+            return Vector3.zero
+                   + (index % orderCount * garbageGap * Vector3.up)
+                   + (index / orderCount * garbageGap * Vector3.back);
+        }
     }
 
     public (bool isContained, GarbageObject garbageObject) OnTrashBins(GarbageType garbageType)
