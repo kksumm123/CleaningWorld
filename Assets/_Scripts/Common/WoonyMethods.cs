78472126e037167d9ffc7d6a49a2cfea09f3ef60
kksumm123 <kksumm123@gmail.com>
2022-08-13 18:17:20 +0900

    [기능] 기본 우니메소드 추가

---
+++ Assets/_Scripts/Common/WoonyMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WoonyMethods
+{
+    public static void Assert<T>(T caller, params (UnityEngine.Object variable, string variableName)[] variables) where T : MonoBehaviour
+    {
+        foreach (var (variable, variableName) in variables)
+        {
+            Debug.Assert(variable != null, $"{variableName} is null", caller);
+        }
+    }
+
+    public static void Assert(string caller, params (UnityEngine.Object variable, string variableName)[] variables)
+    {
+        foreach (var (variable, variableName) in variables)
+        {
+            Debug.Assert(variable != null, $"{caller} : {variableName} is null");
+        }
+    }
+}
