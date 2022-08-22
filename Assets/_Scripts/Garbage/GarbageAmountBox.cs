8b0864edbab7eb9a308df32437e57cc7a7b048d5
kksumm123 <kksumm123@gmail.com>
2022-08-22 23:58:59 +0900

    [기능] 쓰레기 개수를 알려주는 GarbageAmountBox 추가

---
+++ Assets/_Scripts/Garbage/GarbageAmountBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GarbageAmountBox : MonoBehaviour
+{
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] TextMeshProUGUI count;
+
+    public void Initialize()
+    {
+        WoonyMethods.Assert(this, (canvasGroup, nameof(canvasGroup)),
+                                  (count, nameof(count)));
+        Disappear();
+    }
+
+    private void Disappear()
+    {
+        canvasGroup.alpha = 0;
+    }
+
+    public void UpdateAmount(int amount)
+    {
+        if (amount <= 0)
+        {
+            Disappear();
+            return;
+        }
+
+        count.text = $"x{amount}";
+    }
+}
