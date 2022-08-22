c986bf22064b54f6df27f8e464b00afceabb19d7
kksumm123 <kksumm123@gmail.com>
2022-08-23 01:17:44 +0900

    [기능] BillboardObject 기능 추가 및 AmountBox에 적용

---
+++ Assets/_Scripts/Common/BillboardObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BillboardObject : MonoBehaviour
+{
+    Transform mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        transform.rotation = mainCamera.rotation;
+    }
+}
